3df7cae2fcaab7fe66a1bb43abf33ca279c659e1
denyyash <iamyashp.in@gmail.com>
2024-04-26 19:52:54 +0530

    first commit

---
+++ contact.php
@@ -0,0 +1,28 @@
+<?php
+  $title = "Contact";
+  require_once "./template/header.php";
+?>
+    <div class="row">
+        <div class="col-md-3"></div>
+		<div class="col-md-6 text-center">
+			<form class="form-horizontal">
+			  	<fieldset>
+				    <legend>Contact</legend>
+				    <p class="lead">Please feel free to reach out to us using the contact details provided below or through the contact form.</p>
+				Contact Information:<br>
+
+Company Name: Yash Books<br>
+Address: 123 Main Street, Cityville, Country<br>
+Phone: +91 98 4568 7890<br>
+Email: yashonline@gmail.com<br>
+Hours of Operation: Monday - Friday: 9:00 AM - 5:00 PM (IST)<br>
+
+				    
+			  	</fieldset>
+			</form>
+		</div>
+		<div class="col-md-3"></div>
+    </div>
+<?php
+  require_once "./template/footer.php";
+?>
